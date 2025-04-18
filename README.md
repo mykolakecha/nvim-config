96030c9c1094b5cb568075d548f116ae8c03766b
Mykola Kecha <mykola.kecha@namecheap.com>
2025-04-18 14:20:00 +0300

    Added readme & some configuration changes

---
+++ README.md
@@ -0,0 +1,5 @@
+# My NeoVim configuration
+
+## Dependencies
+* "FiraCode Nerd Font" - to install on the system
+* `sudo apt install ripgrep` - for grep to work on Telescope
---
+++ lua/plugins/theme.lua
@@ -0,0 +1,9 @@
+return {
+  -- "Mofiqul/dracula.nvim",
+  "sainnhe/everforest",
+  config = function()
+	vim.g.everforest_background = "hard"
+  	vim.cmd.colorscheme("everforest")
+  end,
+}
+
